c4e9650d03cac02d5f536e1f8e912a745e3f5846
Laszlo Paillat <laszlo.paillat@gmail.com>
2024-10-10 22:40:30 +0200

    some more cleanup

--- Release.bat
+++ Release.bat
@@ -7,6 +7,6 @@ dotnet test source -c Release -r build -l trx
 
 IF %ERRORLEVEL% GTR 0 GOTO :end
 
-dotnet pack source -c Release -o build /p:LOCAL_VERSION=true
+dotnet pack source -c Release -o build /p:Version=0-local%Date:~6,4%%Date:~3,2%%Date:~0,2%%Time:~0,2%%Time:~3,2%%Time:~6,2% /p:SignAssembly=true
 
 :end
